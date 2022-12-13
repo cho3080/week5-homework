5eecedc11e92a2bd507e528a2dae94e934232968
Saehwa95 <tpghk1002@naver.com>
2022-12-13 20:20:50 +0900

    게시글 전체 조회 및 작성 완료

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import styled from "styled-components";
 import TodoCard from "../elements/TodoCard";
+import { __getTodos } from "../redux/modules/todoSlice";
 
 const TodoList = () => {
+  const dispatch = useDispatch();
+  const { isLoading, error, todos } = useSelector((state) => state.todos);
+
+  useEffect(() => {
+    dispatch(__getTodos());
+  }, [dispatch]);
+
+  if (isLoading) {
+    return <div>로딩중....</div>;
+  }
+
+  if (error) {
+    return <div>확인하기 어려운 에러가 발생했습니다.</div>;
+  }
   return (
     <ListWrap>
-      <TodoCard />
+      <BoxWrap>
+        {todos?.map((todo) => (
+          <TodoCard key={`card${todo.id}`} todo={todo} />
+        ))}
+      </BoxWrap>
     </ListWrap>
   );
 };
@@ -15,4 +35,10 @@ export default TodoList;
 const ListWrap = styled.div`
   margin-top: 20px;
   width: 1200px;
+`;
+
+const BoxWrap = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-around;
 `;
--- src/redux/modules/todoSlice.js
+++ src/redux/modules/todoSlice.js
@@ -1,15 +1,74 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
-  todos: [{ id: "", title: "", body: "", comment: [] }],
+  // { id: "", title: "", body: "", comment: [] }
+  todos: [],
+  isLoading: false,
+  error: null,
 };
+
+export const __getTodos = createAsyncThunk(
+  "todos/getTodos",
+  async (payload, thunkAPI) => {
+    try {
+      const todos = await axios.get("http://localhost:3001/todos");
+      console.log(todos.data);
+      return thunkAPI.fulfillWithValue(todos.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const __addTodo = createAsyncThunk(
+  "todos/addTodo",
+  async (payload, thunkAPI) => {
+    console.log(payload);
+    try {
+      const todo = await axios.post("http://localhost:3001/todos", payload);
+      return thunkAPI.fulfillWithValue(todo.data);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
 
 const todoSlice = createSlice({
   name: "todos",
   initialState,
   reducers: {
     addTodo: (state, action) => {
-      state.value = state.value + action.payload;
+      //       state.todos = action.payload; 아래 코드와 차이 ?
+      // console.log(action);
+      state.todos = action.payload;
+    },
+  },
+  extraReducers: {
+    //__getTodos
+    [__getTodos.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [__getTodos.fulfilled]: (state, action) => {
+      console.log(action);
+      state.isLoading = false;
+      state.todos = action.payload;
+    },
+    [__getTodos.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    //__addTodo
+    [__addTodo.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [__addTodo.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.todos.push(action.payload);
+    },
+    [__addTodo.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
     },
   },
 });
